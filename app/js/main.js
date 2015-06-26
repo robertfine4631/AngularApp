87a8d16676757f025cfc4e3cf6ed87d1631173b0
robert fine <robertfine4631@icloud.com>
2015-06-26 12:55:50 -0400

    linked to parse

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,17 @@
-console.log('Angular Rocks', angular);
+;( function (){
+
+	'use strict';
+
+angular.module('coins', ['ngRoute'])
+
+.constant('PARSE', {
+	URL: 'https://api.parse.com/1/',
+	CONFIG: {
+		headers: {
+			'X-Parse-Application-Id' : 'gZcSfY6q4Ye2GPKlHJ3518HsvIZleOkG0ViORhU1',
+			'X-Parse-REST-API-Key' : 'o8JsbLYim5gZq1rCNevzuJmbKUTrXhbSDD52qKlq'
+		}
+	}
+})
+
+}())
