a32843915d0e919dd816c7a64a6df7c5710a84fa
robert fine <robertfine4631@icloud.com>
2015-07-13 19:58:21 -0400

    adding basic routes

--- app/js/main.js
+++ app/js/main.js
@@ -2,16 +2,42 @@
 
   'use strict';
 
-  angular.module('app', ['ngRoute'])
+  angular.module('app', ['ngRoute', 'ngCookies'])
 
   .constant('PARSE', {
     URL: 'https://api.parse.com/1/',
     CONFIG: {
       headers: {
 
-        'X-Parse-Application-Id' : '',
-        'X-Parse-REST-API-Key' : '',
+        'X-Parse-Application-Id' : 'Oe2pr6X4ikeLrUZ5DW8ztm1FdubaMrGC40skZZel',
+        'X-Parse-REST-API-Key' : 'Mjdzx3qmyLWY7nrIpNWyuvHHmoTxKUvkXokwBLtR',
       }
     }
   })
+
+
+  .config(function ($routeProvider){
+    $routeProvider
+
+    .when('/', {
+      templateUrl: 'js/register/register.tpl.html',
+      controller: 'Register'
+    })
+
+    .when('/home',{
+      templateUrl: '',
+      controller: ''
+    })
+
+    .when('/distance_today',{
+      templateUrl: '',
+      controller: ''
+    })
+
+    .when('/total_distance', {
+      templateUrl: '',
+      controller: ''
+    });
+  })
+
 }());
