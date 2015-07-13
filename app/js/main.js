072b05f76a6de5e656eba2cf64a060c70a640c20
robert fine <robertfine4631@icloud.com>
2015-07-13 19:44:13 -0400

    added angular cookies

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,17 @@
 :(function (){
 
   'use strict';
+
+  angular.module('app', ['ngRoute'])
+
+  .constant('PARSE', {
+    URL: 'https://api.parse.com/1/',
+    CONFIG: {
+      headers: {
+
+        'X-Parse-Application-Id' : '',
+        'X-Parse-REST-API-Key' : '',
+      }
+    }
+  })
 }());
