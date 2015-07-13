8e634b484858cdab5e51ea9b5635bf83a43b5720
robert fine <robertfine4631@icloud.com>
2015-07-13 19:28:46 -0400

    restarting crud

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,30 @@
 ;( function (){
 
-	'use strict';
+  'use strict';
 
 angular.module('coins', ['ngRoute'])
 
 .constant('PARSE', {
-	URL: 'https://api.parse.com/1/',
-	CONFIG: {
-		headers: {
-			'X-Parse-Application-Id' : 'gZcSfY6q4Ye2GPKlHJ3518HsvIZleOkG0ViORhU1',
-			'X-Parse-REST-API-Key' : 'o8JsbLYim5gZq1rCNevzuJmbKUTrXhbSDD52qKlq'
-		}
-	}
+  URL: 'https://api.parse.com/1/',
+  CONFIG: {
+    headers: {
+      'X-Parse-Application-Id' : 'gZcSfY6q4Ye2GPKlHJ3518HsvIZleOkG0ViORhU1',
+      'X-Parse-REST-API-Key' : 'o8JsbLYim5gZq1rCNevzuJmbKUTrXhbSDD52qKlq'
+    }
+  }
 })
 
-}())
+.config( [ '$routeProvider',
+  function ($routeProvider) {
+    
+    $routeProvider.when('/AddACOIN', {
+      controller: 'coins',
+      templateUrl: 'js/coin/addCoin.tpl.html'
+
+    });
+  
+
+    }
+  ]);
+
+}());
