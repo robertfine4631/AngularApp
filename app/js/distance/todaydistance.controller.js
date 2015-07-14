82b122efe719b94ae9ece7c5c17de2ce65242958
robert fine <robertfine4631@icloud.com>
2015-07-14 16:18:44 -0400

    adding objects to parse

--- app/js/distance/todaydistance.controller.js
+++ app/js/distance/todaydistance.controller.js
@@ -3,10 +3,19 @@
 
   angular.module('app')
 
-  .controller('Today', ['$scope', '$http',
-    function ($scope, $http){
+  .controller('Today', ['$scope', '$http', '$cookies','PARSE',
+    function ($scope, $http, $cookies, PARSE){
 
+      $scope.user = $cookies.get('username');
 
+        var distance = function (options) {
+          this.distance = options.distance;
+        };
+      $scope.distance = function (data) {
+        $http.post(PARSE.URL + 'classes/distance', data, PARSE.CONFIG).success( function (data){
+          console.log(data);
+        });
+      };
 
     }
     ]);
