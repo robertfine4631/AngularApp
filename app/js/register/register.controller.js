ba442a5ba181a715c90b350b0aafb9078defb846
robert fine <robertfine4631@icloud.com>
2015-07-13 20:32:34 -0400

    registration controller is complete

--- app/js/register/register.controller.js
+++ app/js/register/register.controller.js
@@ -6,7 +6,23 @@
   .controller('Register', ['$scope', '$http', 'PARSE', '$location', '$cookies',
     function ($scope, $http, PARSE, $location, $cookies){
 
-      var register
+      var register = function (options) {
+        this.username = options.username;
+        this.password= options.password;
+      };
+
+      $scope.registration = function (data) {
+        $http.post(PARSE.URL + 'users', data, PARSE.CONFIG).success( function (data){
+          console.log(data);
+
+          $cookies.put('sessionToken', data.sessionToken);
+          $cookies.put('username', data.username);
+
+          $location.path('/home');
+
+          $scope.registration = {};
+        });
+      };
 
     }
     ]);
